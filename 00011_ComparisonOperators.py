fff16838e54a051de52a3c71033c8de4519dddd5
mathisudar <76192299+mathisudar@users.noreply.github.com>
2022-06-04 19:00:28 +0530

    Create 00011_ComparisonOperators.py

---
+++ 00011_ComparisonOperators.py
@@ -0,0 +1,32 @@
+# Comparison Operators or Relational Operators
+
+"""
+==	Equal
+!=	Not equal
+>	Greater than
+<	Less than
+>=	Greater than or equal to
+<=	Less than or equal to
+
+"""
+
+
+a = 20
+b = 20
+print(a == b)
+print(a != b)
+print(a > b)
+print(a < b)
+print(a >= b)
+print(a <= b)
+
+
+
+# Output
+
+True
+False
+False
+False
+True
+True
