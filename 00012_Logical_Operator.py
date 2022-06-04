b81acab8ffdda46fbe34156bfa3c0b2b7caca0da
mathisudar <76192299+mathisudar@users.noreply.github.com>
2022-06-04 19:02:16 +0530

    Create 00012_Logical_Operator.py

---
+++ 00012_Logical_Operator.py
@@ -0,0 +1,22 @@
+# Logical Operators in Python
+
+"""
+and
+or
+not
+
+"""
+
+
+a = 25
+print(a >= 10 and a <= 20)
+print(a >= 10 or a <= 20)
+print(not(a >=  10 and a <= 20))
+
+
+
+# Output
+
+False
+True
+True
