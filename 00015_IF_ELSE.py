161bd8a1ac751d0231e3f7226d633dafaf9cca2d
mathisudar <76192299+mathisudar@users.noreply.github.com>
2022-06-04 19:08:51 +0530

    Create 00015_IF_ELSE.py

---
+++ 00015_IF_ELSE.py
@@ -0,0 +1,17 @@
+# IF Else Statement in Python
+
+name = input("Enter Your Name : ")
+age = int(input("Enter Your Age : "))
+if age >= 18:
+    print(name, " age is ", age, " Eligible for Vote.")
+else:
+    print(name, " age is ", age, " Not Eligible for Vote.")
+
+    
+    
+# Output
+
+Enter Your Name : Ram
+Enter Your Age : 23
+Ram  age is  23  Eligible for Vote.
+
