e4b81fd40c743766beed5cd485c3fae6c57adc87
mathisudar <76192299+mathisudar@users.noreply.github.com>
2022-06-04 10:20:09 +0530

    Create 00006_TypeCasting.py

---
+++ 00006_TypeCasting.py
@@ -0,0 +1,24 @@
+# Source Code
+
+"""
+int()
+float()
+str()
+"""
+
+
+
+a = int(input("Enter The Value of A : "))
+b = int(input("Enter The Value of B : "))
+c = a + b
+print("Total : " + str(c))
+
+
+# Output
+
+Enter The Value of A : 20
+Enter The Value of B : 20
+  
+Total : 40
+  
+  
