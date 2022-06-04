38f71a8e0950c87c3db878cd9b78d62fbfe9da5f
mathisudar <76192299+mathisudar@users.noreply.github.com>
2022-06-04 18:58:54 +0530

    Create 00010_AssignmentOperators.py

---
+++ 00010_AssignmentOperators.py
@@ -0,0 +1,44 @@
+# Assignment Operators
+
+
+"""
+=   Assignment
++=	Addition
+-=	Subtraction
+*=	Multiplication
+/=	Division
+%=	Modulus
+**=	Exponentiation
+//=	Floor division
+"""
+
+
+a = 125
+print(a)
+a += 5  # a=a+5
+print(a)
+a -= 10  # a=a-10
+print(a)
+a *= 10  # a=a*10
+print(a)
+a /= 10
+print(a)
+a %= 10
+print(a)
+a **=10
+print(a)
+a //= 10 
+print(a)
+
+
+
+#Output
+
+125
+130
+120
+1200
+120.0
+0.0
+0.0
+0.0
