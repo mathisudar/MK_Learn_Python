7e7396c4a1bb5c9d51503153914cd27208835808
mathisudar <76192299+mathisudar@users.noreply.github.com>
2022-06-04 18:30:01 +0530

    Create 00009_ArithmeticOperators.py

---
+++ 00009_ArithmeticOperators.py
@@ -0,0 +1,35 @@
+# Arithmetic operators
+
+"""
++	Addition
+-	Subtraction
+*	Multiplication
+/	Division
+%	Modulus
+**	Exponentiation
+//	Floor division
+"""
+
+
+a = 123
+b = 10
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b)
+print(2**3)
+
+
+
+
+# Output
+
+133
+113
+1230
+12.3
+12
+3
+8
