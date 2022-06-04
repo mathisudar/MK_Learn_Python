61d0e8ffa0bd94e5c09e86fb0039a49478b3e052
mathisudar <76192299+mathisudar@users.noreply.github.com>
2022-06-04 19:06:14 +0530

    Create 00013_Bitwise_Operators.py

---
+++ 00013_Bitwise_Operators.py
@@ -0,0 +1,35 @@
+
+# Bitwise Operators
+
+"""
+& 	AND
+|	OR
+^	XOR
+~ 	NOT
+<<	Zero fill left shift
+>>	Signed right shift
+"""
+
+
+a = 25
+b = 45
+print(a & b)
+print(a | b)
+print(a ^ b)
+print(~a)
+print(a << 2)
+print(a >> 2)
+
+
+
+
+
+# Output
+
+9
+61
+52
+-26
+100
+6
+
