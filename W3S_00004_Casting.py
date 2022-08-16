0d889638a3e437b28da4c9b4ca42f0d210d43e0e
MATHI <76192299+mathisudar@users.noreply.github.com>
2022-08-16 21:00:01 +0530

    Create W3S_00004_Casting.py

---
+++ W3S_00004_Casting.py
@@ -0,0 +1,27 @@
+
+x = int(1)
+y = int(2.8)
+z = int("3")
+print(x) # 1
+print(y) # 2
+print(z) # 3
+
+
+x = float(1)
+y = float(2.8)
+z = float("3")
+w = float("4.2")
+print(x) # 1.0
+print(y) # 2.8
+print(z) # 3.0
+print(w) # 4.2
+
+
+
+x = str("s1")
+y = str(2)
+z = str(3.0)
+print(x) # s1
+print(y) # 2
+print(z) # 3.0
+
