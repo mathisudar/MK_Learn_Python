39d9e7c02e1296ce19dce3f8943afbebc612a32a
MATHI <76192299+mathisudar@users.noreply.github.com>
2022-08-16 20:49:11 +0530

    Create W3S_00002_DataTypes.py

---
+++ W3S_00002_DataTypes.py
@@ -0,0 +1,284 @@
+x = "Hello World" # str
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x))  # str
+
+
+x = str("Hello World")
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) # str
+
+
+
+
+x = 20 # int
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x))  # int
+
+
+x = int(20)
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) # int
+
+
+
+x = 20.5 # float
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) # float
+
+
+x = float(20.5)
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) # float
+
+
+
+
+x = 1j # Complex
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) # Complex
+
+
+x = complex(1j)
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) # Complex
+
+
+
+
+x = ["apple", "banana", "cherry"] # List
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x))  # List
+
+
+x = list(("apple", "banana", "cherry"))
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) # List
+
+
+
+x = ("apple", "banana", "cherry") # Tuple
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) # Tuple
+
+
+x = tuple(("apple", "banana", "cherry"))
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) # Tuple
+
+
+
+x = range(6) # Range
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x))  # Range
+
+
+x = range(6)
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x))  # Range
+
+
+
+
+x = {"name" : "John", "age" : 36} # Dict
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) # Dict
+
+
+x = dict(name="John", age=36)
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) # Dict
+
+
+
+
+x = {"apple", "banana", "cherry"} # Set
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x))  # Set
+
+
+x = set(("apple", "banana", "cherry")) # Set
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+
+
+x = frozenset({"apple", "banana", "cherry"}) # Frozen Set
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) # frozenset
+
+
+x = frozenset(("apple", "banana", "cherry"))
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) # frozenset
+
+
+
+x = True # bool
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x))  # bool
+
+
+x = bool(5)
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) # bool
+
+
+
+x = b"Hello" # bytes
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) # bytes
+
+
+x = bytes(5)
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) # bytes
+
+
+
+
+x = bytearray(5) # bytearray
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) # bytearray
+
+
+x = bytearray(5)
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) # bytearray
+
+
+
+x = memoryview(bytes(5)) # memoryview
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) # memoryview
+
+
+x = memoryview(bytes(5))
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) # memoryview
+
+
+x = None # none
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) # none
+
+
+
+
+
+
