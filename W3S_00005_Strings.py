e58b511a1a506383f31a706019c3063d660468fe
MATHI <76192299+mathisudar@users.noreply.github.com>
2022-08-19 09:59:45 +0530

    Update W3S_00005_Strings.py

--- W3S_00005_Strings.py
+++ W3S_00005_Strings.py
@@ -155,3 +155,40 @@ print(myorder.format(quantity, itemno, price)) # I want to pay 49.95 dollars for
 
 txt = "We are the so-called \"Vikings\" from the north."
 print(txt)  # We are the so-called "Vikings" from the north.
+
+
+txt = 'It\'s alright.'
+print(txt)  # It's alright.
+
+txt = "This will insert one \\ (backslash)."
+print(txt)  # This will insert one \ (backslash).
+
+txt = "Hello\nWorld!"
+print(txt) 
+
+# Hello
+# World!
+
+txt = "Hello\tWorld!"
+print(txt)  # Hello   World!
+
+#This example erases one character (backspace):
+txt = "Hello \bWorld!"
+print(txt)  # HelloWorld!
+
+
+#A backslash followed by three integers will result in a octal value:
+txt = "\110\145\154\154\157"
+print(txt)  # Hello
+
+
+#A backslash followed by an 'x' and a hex number represents a hex value:
+txt = "\x48\x65\x6c\x6c\x6f"
+print(txt) # Hello
+
+
+
+
+
+
+
