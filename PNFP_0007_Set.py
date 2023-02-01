5d51748e871093461464ab6129ae19afe34584eb
Mathi Krishnan <76192299+mathisudar@users.noreply.github.com>
2023-02-01 19:13:56 +0530

    Create PNFP_0007_Set.py

---
+++ PNFP_0007_Set.py
@@ -0,0 +1,15 @@
+first_names = {'Adam',"Beth","Charlie"}
+
+for name in first_names:
+    print(name)
+
+print("\n")   
+thisset = {"apple", "banana", "cherry"}
+
+for x in thisset:
+  print(x)    
+
+  
+print("\n")
+set1 = {"abc", 34, True, 40, "male"}  
+print(type(set1))
