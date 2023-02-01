f3f05b9b2f1151c3a0a96df2eeed8f7af06d1151
Mathi Krishnan <76192299+mathisudar@users.noreply.github.com>
2023-02-01 19:35:30 +0530

    Create PNFP_0015_FrozenSet.py

---
+++ PNFP_0015_FrozenSet.py
@@ -0,0 +1,15 @@
+# Frozen Sets - They are immutable and new elements cannot added after its defined.
+
+b = frozenset('asdfagsa')
+print(b)
+
+"""
+frozenset({'g', 'a', 'f', 'd', 's'})
+"""
+
+cities = frozenset(["Frankfurt", "Basel","Freiburg"])
+print(cities)
+
+"""
+frozenset({'Freiburg', 'Basel', 'Frankfurt'})
+"""
