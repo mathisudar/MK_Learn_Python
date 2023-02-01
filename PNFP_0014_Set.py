b7643236993892f492c75c006158cbcff5b8bdc1
Mathi Krishnan <76192299+mathisudar@users.noreply.github.com>
2023-02-01 19:31:17 +0530

    Create PNFP_0014_Set.py

---
+++ PNFP_0014_Set.py
@@ -0,0 +1,27 @@
+basket = {'apple', 'orange', 'apple', 'pear', 'orange', 'banana'}
+print(basket) # duplicates will be removed
+
+"""
+
+{'apple', 'orange', 'pear', 'banana'}
+
+"""
+
+a = set('abracadabra')
+print(a) # unique letters in a
+
+"""
+{'a', 'r', 'c', 'd', 'b'}
+Sets are unordered collections of unique objects,
+
+"""
+
+
+a.add('z')
+print(a)
+
+"""
+
+{'a', 'r', 'c', 'z', 'd', 'b'}
+
+"""
