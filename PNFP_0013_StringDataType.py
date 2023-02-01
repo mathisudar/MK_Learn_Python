d7676628e91489cefad56216310c7a1a4b7db18c
Mathi Krishnan <76192299+mathisudar@users.noreply.github.com>
2023-02-01 19:25:14 +0530

    Create PNFP_0013_StringDataType.py

---
+++ PNFP_0013_StringDataType.py
@@ -0,0 +1,13 @@
+# Section 2.1: String Data Type
+
+a_str = 'Hello World'
+print(a_str) #output will be whole string. Hello World
+print(a_str[0]) #output will be first character. H
+print(a_str[0:5]) #output will be first five characters. Hello
+
+
+"""
+Hello World
+H
+Hello
+"""
